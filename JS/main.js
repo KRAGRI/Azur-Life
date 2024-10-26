bb1197c8674537daff46832592e3d1175ecae099
KRAGRI <knugacom2020@gmail.com>
2024-10-26 19:38:28 +0400

    commit-5

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,23 @@
 import { language } from "./language.js";
 language();
+
+const btnMenu = document.querySelector(".header__menu");
+const backgroundMenu = document.querySelector(".menu");
+const freedomMenu = document.querySelector(".menu__freedom");
+const headerRigth = document.querySelector(".header__rigth");
+
+btnMenu.addEventListener("click", menu);
+freedomMenu.addEventListener("click", menu);
+function menu() {
+  if (btnMenu.classList.contains("header__menu-active")) {
+    btnMenu.classList.remove("header__menu-active");
+    headerRigth.classList.remove("header__rigth-active");
+    document.body.style.overflow = "visible";
+    backgroundMenu.style.display = "none";
+  } else {
+    btnMenu.classList.add("header__menu-active");
+    headerRigth.classList.add("header__rigth-active");
+    document.body.style.overflow = "hidden";
+    backgroundMenu.style.display = "flex";
+  }
+}
